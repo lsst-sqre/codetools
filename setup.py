f21b4b7abb1ae409da4a5dea1f08257bfaa92b13
Ross Ceballo <rocebxyz@gmail.com>
2024-11-19 17:40:02 -0500

    remove travis files

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ import os
 
 from setuptools import find_packages, setup
 
-PACKAGENAME = "sqre-codekit"
-DESCRIPTION = "LSST Data Management SQuaRE code management tools"
-AUTHOR = "Frossie Economou"
-AUTHOR_EMAIL = "frossie@lsst.org"
-URL = "https://github.com/lsst-sqre/sqre-codekit"
-LICENSE = "MIT"
+PACKAGENAME = 'sqre-codekit'
+DESCRIPTION = 'LSST Data Management SQuaRE code management tools'
+AUTHOR = 'Frossie Economou'
+AUTHOR_EMAIL = 'frossie@lsst.org'
+URL = 'https://github.com/lsst-sqre/sqre-codekit'
+LICENSE = 'MIT'
 
 
 def read(filename):
     """Convenience function for includes"""
     full_filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
-    return codecs.open(full_filename, "r", "utf-8").read()
+    return codecs.open(full_filename, 'r', 'utf-8').read()
 
 
-long_description = read("README.md")  # pylint:disable=invalid-name
+long_description = read('README.md')  # pylint:disable=invalid-name
 
 setup(
     name=PACKAGENAME,
@@ -31,42 +31,43 @@ setup(
     author_email=AUTHOR_EMAIL,
     license=LICENSE,
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: MIT License",
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
     ],
-    keywords="lsst",
+    keywords='lsst',
     use_scm_version=True,
-    packages=find_packages(exclude=["docs", "tests*"]),
+    packages=find_packages(exclude=['docs', 'tests*']),
     install_requires=[
-        "MapGitConfig==1.1",
-        "progressbar2==3.37.1",
-        "public==1.0",
-        "pygithub==1.59",
-        "pyyaml>=5.1",
-        "requests>=2.8.1,<3.0.0",
+        'MapGitConfig==1.1',
+        'progressbar2==3.37.1',
+        'public==1.0',
+        'pygithub==1.59',
+        'pyyaml>=5.1',
+        'requests>=2.8.1,<3.0.0',
     ],
     setup_requires=[
-        "pytest-runner>=4.4,<5",
-        "setuptools_scm",
+        'pytest-runner>=4.4,<5',
+        'setuptools_scm',
     ],
     tests_require=[
-        "flake8",
-        "pytest<5",
-        # "pytest-flake8",
-        "responses>=0.9.0,<1",
+        'flake8',
+        'pytest<5',
+        # Removed due to incompatibility
+        # 'pytest-flake8',
+        'responses>=0.9.0,<1',
     ],
     # package_data={},
     entry_points={
-        "console_scripts": [
-            "github-auth = codekit.cli.github_auth:main",
-            "github-decimate-org = codekit.cli.github_decimate_org:main",
-            "github-fork-org = codekit.cli.github_fork_org:main",
-            "github-get-ratelimit= codekit.cli.github_get_ratelimit:main",
-            "github-list-repos = codekit.cli.github_list_repos:main",
-            "github-mv-repos-to-team = codekit.cli.github_mv_repos_to_team:main",  # NOQA
-            "github-tag-release = codekit.cli.github_tag_release:main",
-            "github-tag-teams = codekit.cli.github_tag_teams:main",
+        'console_scripts': [
+            'github-auth = codekit.cli.github_auth:main',
+            'github-decimate-org = codekit.cli.github_decimate_org:main',
+            'github-fork-org = codekit.cli.github_fork_org:main',
+            'github-get-ratelimit= codekit.cli.github_get_ratelimit:main',
+            'github-list-repos = codekit.cli.github_list_repos:main',
+            'github-mv-repos-to-team = codekit.cli.github_mv_repos_to_team:main',  # NOQA
+            'github-tag-release = codekit.cli.github_tag_release:main',
+            'github-tag-teams = codekit.cli.github_tag_teams:main',
         ]
     },
 )
